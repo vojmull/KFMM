c51f03b3c229dd3bbf75182a734e6053ea98620e
vojmull <mueller.vojtech@gmail.com>
2019-01-05 21:44:01 +0100

    uprava-datovy-typ
    ++

--- API/MessengerApi/MessengerApi/Models/Users.cs
+++ API/MessengerApi/MessengerApi/Models/Users.cs
@@ -12,7 +12,7 @@ namespace MessengerApi
         public int Id { get; set; }
         public string Name { get; set; }
         public string Surname { get; set; }
-        public DateTime Birthdate { get; set; }
+        public string Birthdate { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
         public string ImageServerPath { get; set; }
