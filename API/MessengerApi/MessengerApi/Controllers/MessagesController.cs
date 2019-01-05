9a94a27f8896e5b5a92bfc72e110acaba07d6c25
tomaskorinek <gigousek.k@gmail.com>
2019-01-05 16:00:53 +0100

    API_zabezpecení messages

--- API/MessengerApi/MessengerApi/Controllers/MessagesController.cs
+++ API/MessengerApi/MessengerApi/Controllers/MessagesController.cs
@@ -18,10 +18,21 @@ namespace MessengerApi.Controllers
         private string _imgStorage = @"D:\vojta\Knihovny\Desktop\git_desktop\KFMM\API\MessengerApi\MessengerApi" + "/_imgStorage/";
 
         // Vraci posledních X konverzaci (parametr) s obsahem jedne (posledni zpravy) => pro prehled vsech konverzaci
-        [System.Web.Http.Route("api/messages/{userId}-{conversationCnt}")]
+        [System.Web.Http.Route("api/messages/{token}-{userId}-{conversationCnt}")]
         [System.Web.Http.HttpGet]
-        public string GetMessages(int userId, int conversationCnt)
+        public string GetMessages(string token,int userId, int conversationCnt)
         {
+            Token t = Token.Exists(token);
+            if (t == null)
+            {
+                //token není v databázi  
+                return "ERROR";
+            }
+            if (!t.IsUser)
+            {
+                //token nepatří userovi
+                return "ERROR";
+            }
             List<Conversations> Conversations = this._database.Conversations.Where(c => c.IdUser1 == userId || c.IdUser2 == userId).Take(conversationCnt).ToList<Conversations>();
             foreach (Conversations item in Conversations)
             {
@@ -38,10 +49,21 @@ namespace MessengerApi.Controllers
             return JsonConvert.SerializeObject(Conversations);
         }
 
-        [System.Web.Http.Route("api/messages/getimage/{imgPath}")]
+        [System.Web.Http.Route("api/messages/getimage/{token}-{imgPath}")]
         [System.Web.Http.HttpGet]
-        public HttpResponseMessage GetImage(string imgPath)
+        public HttpResponseMessage GetImage(string token,string imgPath)
         {
+            Token t = Token.Exists(token);
+            if (t == null)
+            {
+                //token není v databázi  
+                return null; //error
+            }
+            if (!t.IsUser)
+            {
+                //token nepatří userovi  
+                return null; //error
+            }
             HttpResponseMessage response = new HttpResponseMessage();
             response.Content = new StreamContent(new FileStream(this._imgStorage + imgPath + ".png", FileMode.Open)); // this file stream will be closed by lower layers of web api for you once the response is completed.
             response.Content.Headers.ContentType = new MediaTypeHeaderValue("image/png");
