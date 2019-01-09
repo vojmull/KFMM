510aefea3e5eb8dfefabd407f9d1bf8bcde0c620
Vojtěch Musílek <36379818+vojtechmusilek@users.noreply.github.com>
2019-01-09 11:35:47 +0100

    x
    x

---
+++ API/MessengerApi/MessengerApi/Controllers/ProfilesController.cs
@@ -0,0 +1,35 @@
+﻿using MessengerApi.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using System.Web.Mvc;
+
+namespace MessengerApi.Controllers
+{
+    public class ProfilesController : ApiController
+    {
+        private dbContext _database = new dbContext();
+
+        [System.Web.Http.Route("api/profiles/{token}-{userId}")]
+        [System.Web.Http.HttpGet]
+        public string GetProfiles(string token, int userId)
+        {
+            Token t = Token.Exists(token);
+            if (t == null)
+            {
+                //token není v databázi  
+                return "ERROR";
+            }
+            if (!t.IsUser)
+            {
+                //token nepatří userovi
+                return "ERROR";
+            }
+
+            return JsonConvert.SerializeObject(this._database.Users.Where(u => u.Id == userId).FirstOrDefault());
+        }
+    }
+}
