d596c4c5e11e4285fabac3f70925ca0b3553dec7
tomaskorinek <gigousek.k@gmail.com>
2019-01-15 20:26:08 +0100

    AndroidApp_design, conversation, autorefresh

--- AndroidApp_KFM_messenger/app/src/main/java/com/sssvt_prg/tomas/kfm_messenger/CustomListView_conversations.java
+++ AndroidApp_KFM_messenger/app/src/main/java/com/sssvt_prg/tomas/kfm_messenger/CustomListView_conversations.java
@@ -3,6 +3,8 @@ package com.sssvt_prg.tomas.kfm_messenger;
 import android.app.Activity;
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.support.v4.content.ContextCompat;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -34,6 +36,7 @@ public class CustomListView_conversations extends BaseAdapter {
         mInflater = (LayoutInflater) context.getSystemService(context.LAYOUT_INFLATER_SERVICE);
 
 
+
     }
 
     @Override
@@ -57,14 +60,30 @@ public class CustomListView_conversations extends BaseAdapter {
     public View getView(int position, View convertView, ViewGroup parent) {
         View v = mInflater.inflate(R.layout.conversation_listview_detail,null);
 
-        if(authors.get(position).equals(LoginActivity.UserID)) {
+        /*if(authors.get(position).equals(LoginActivity.UserID)) {
             v.setBackgroundColor(LoginActivity.newColor);
 
-        }
+        }*/
 
         TextView conversationTV = (TextView) v.findViewById(R.id.message_detail_textView);
         TextView timeTV = (TextView) v.findViewById(R.id.time_detail_textView);
         ImageView imageIV = (ImageView) v.findViewById(R.id.seen_detail_imageView);
+
+        /*if(authors.get(position).equals(LoginActivity.UserID)) {
+            v.setBackgroundColor(LoginActivity.newColor);
+
+        }*/
+        if(authors.get(position).equals(LoginActivity.UserID)) {
+            //conversationTV.setBackgroundColor(LoginActivity.newColor);
+
+            conversationTV.setBackground(ContextCompat.getDrawable(context, R.drawable.rounded_corner_darker));
+
+            //conversationTV.setGravity(Gravity.END);
+            conversationTV.setGravity(Gravity.END);
+        }
+        else{
+
+        }
 
         String conv = conversations.get(position);
         String tim = times.get(position);
@@ -76,10 +95,32 @@ public class CustomListView_conversations extends BaseAdapter {
 
         if(seen.get(position)==true)
         {
-            imageIV.setImageResource(R.drawable.seen);
+            try{
+                if(seen.get(position+1).equals(false)){
+                    imageIV.setImageResource(R.drawable.seen);
+                }
+                else{
+                    imageIV.setVisibility(View.INVISIBLE);
+                }
+            }
+            catch(Exception ex){
+                imageIV.setImageResource(R.drawable.seen);
+            }
+
         }
         else if(delievered.get(position)==true ||delievered.get(position)==false){
-            imageIV.setImageResource(R.drawable.delievered);
+            try{
+                if(delievered.get(position+1).equals(false)){
+                    imageIV.setImageResource(R.drawable.delievered);
+                }
+                else{
+                    imageIV.setVisibility(View.INVISIBLE);
+                }
+            }
+            catch(Exception ex){
+                imageIV.setImageResource(R.drawable.delievered);
+            }
+
         }
 
 
